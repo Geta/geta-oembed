cd70b23a62ac0f991d3ecc7d6f6d7d3f8fa907c0
Mattias Olsson <mattias@getadigital.com>
2023-11-10 07:55:51 +0100

    Fixed issue with build targets

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,5 @@
 $outputDir = ".\.package\"
-$version = "2.1.0"
+$version = "2.2.0"
 
 dotnet build --configuration Release /p:Version=$version
 dotnet pack  --configuration Release --output $outputDir /p:Version=$version --no-build
